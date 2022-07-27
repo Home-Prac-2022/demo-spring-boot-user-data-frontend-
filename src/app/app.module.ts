2f0717cd6b9422caff5d448401dce1eec2dc0022
ThanuraSilva <thanurasilva1989@gmail.com>
2022-07-27 11:03:29 +0530

    Routing add with components updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    NavBarComponent,
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'',component:SignUpComponent},
+      {path:'signup',component:SignUpComponent},
+      {path:'login',component:LoginComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
