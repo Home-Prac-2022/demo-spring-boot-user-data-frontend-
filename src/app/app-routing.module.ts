992ed5e2982c9d5f144ca18e3d86c9c2b324cfb2
ThanuraSilva <thanurasilva1989@gmail.com>
2022-07-28 16:46:01 +0530

    App Routing Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SignUpComponent} from "./sign-up/sign-up.component";
+import {LoginComponent} from "./login/login.component";
+import {AuthGuard} from "./shared/auth.guard";
+import {UserProfileComponentComponent} from "./user-profile-component/user-profile-component.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/login',pathMatch:'full'},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'login', component: LoginComponent},
+  { path: 'user-profile/:id', component: UserProfileComponentComponent, canActivate: [AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
